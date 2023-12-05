141cdde6dc00f1cb566f2e6edd49fae6a374a7d8
Dimitris <zinodimitris@gmail.com>
2023-12-05 18:07:51 +0100

    Implement inner-level learning procedure of MAML

--- maml.py
+++ maml.py
@@ -19,29 +19,36 @@ class MAML(nn.Module):
 
     # controller input = image + label_previous
     def apply(self, x_supp, y_supp, x_query, y_query, training=False):
-        """
-        Pefrosmt the inner-level learning procedure of MAML: adapt to the given task 
-        using the support set. It returns the predictions on the query set, as well as the loss
-        on the query set (cross-entropy).
-        You may want to set the gradients manually for the base-learner parameters 
+            """
+            Pefrosmt the inner-level learning procedure of MAML: adapt to the given task 
+            using the support set. It returns the predictions on the query set, as well as the loss
+            on the query set (cross-entropy).
+            You may want to set the gradients manually for the base-learner parameters 
 
-        :param x_supp (torch.Tensor): the support input iamges of shape (num_support_examples, num channels, img width, img height)
-        :param y_supp (torch.Tensor): the support ground-truth labels
-        :param x_query (torch.Tensor): the query inputs images of shape (num_query_inputs, num channels, img width, img height)
-        :param y_query (torch.Tensor): the query ground-truth labels
+            :param x_supp (torch.Tensor): the support input iamges of shape (num_support_examples, num channels, img width, img height)
+            :param y_supp (torch.Tensor): the support ground-truth labels
+            :param x_query (torch.Tensor): the query inputs images of shape (num_query_inputs, num channels, img width, img height)
+            :param y_query (torch.Tensor): the query ground-truth labels
 
-        :returns:
-          - query_preds (torch.Tensor): the predictions of the query inputs
-          - query_loss (torch.Tensor): the cross-entropy loss on the query inputs
-        """
-        
-        # TODO: implement this function
+            :returns:
+            - query_preds (torch.Tensor): the predictions of the query inputs
+            - query_loss (torch.Tensor): the cross-entropy loss on the query inputs
+            """
+            init_w = [param.clone() for param in self.network.parameters()]
 
-        # Note: to make predictions and to allow for second-order gradients to flow if we want,
-        # we use a custom forward function for our network. You can make predictions using
-        # preds = self.network(input_data, weights=<the weights you want to use>)
+            for _ in range(self.num_updates):
+                fast_weights = [param.clone() for param in init_w]
 
-        if training:
-            query_loss.backward() # do not remove this if statement, otherwise it won't train
+                support_set_predictions = self.network(x_supp, weights=fast_weights)
+                support_set_loss = self.inner_loss(support_set_predictions, y_supp)
+                gradients = torch.autograd.grad(support_set_loss, fast_weights, create_graph=self.second_order)
 
-        raise NotImplementedError()
+                fast_weights= [weight - self.inner_lr * grad for weight, grad in zip(fast_weights, gradients)]
+
+            query_preds = self.network(x_query, weights=fast_weights)
+            query_loss = self.inner_loss(query_preds, y_query)
+
+            if training:
+                query_loss.backward()
+
+            return query_preds, query_loss
